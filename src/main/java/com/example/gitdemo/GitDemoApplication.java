f16be3a0b526d208721fc3f01846f210b24bbda8
MirasSafadi <45172450+MirasSafadi@users.noreply.github.com>
2024-02-13 20:11:32 +0200

    Update GitDemoApplication.java

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -9,7 +9,7 @@ public class GitDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitDemoApplication.class, args);
         System.out.println("Hello World!");
-        System.out.println("Hello...");
+        System.out.println("Hello...!!!");
     }
 
 }
